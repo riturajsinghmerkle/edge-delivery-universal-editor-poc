0a6872dda640827d9acba1113e3e62609a57c436
riturajsinghmerkle <rituraj.singh@merkle.com>
2024-10-19 01:27:02 +0200

    Iframe Block component changed logic

--- blocks/iframe-app/iframe-app.js
+++ blocks/iframe-app/iframe-app.js
@@ -1,7 +1,3 @@
-import { getPathDetails } from '../../scripts/scripts.js';
-
-
-
 export default function decorate(block) {
   const link = block.querySelector('a');
   const appUrl = link ? link.getAttribute('href') : block.textContent.trim();
@@ -9,7 +5,7 @@ export default function decorate(block) {
   iframe.src = "https://publish-p139364-e1423304.adobeaemcloud.com/content/celestyal-asset-share/us/en/light.html";
   iframe.width = '100%';
   iframe.height = '500px';
-  iframe.style.border = 'none';
+  iframe.style.border = '3px solid black';
   block.textContent = '';
   block.append(iframe);
 }
