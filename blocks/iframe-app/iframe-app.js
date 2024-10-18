7d17c29e4b6deeb79d96eb9c6bedbb70dfca518d
riturajsinghmerkle <rituraj.singh@merkle.com>
2024-10-19 00:07:05 +0200

    Iframe Block component changed logic

--- blocks/iframe-app/iframe-app.js
+++ blocks/iframe-app/iframe-app.js
@@ -51,7 +51,7 @@ export default function decorate(block) {
   const link = block.querySelector('a');
   const appUrl = link ? link.getAttribute('href') : block.textContent.trim();
   const iframe = document.createElement('iframe');
-  iframe.src = appUrl;
+  iframe.src = "https://publish-p139364-e1423304.adobeaemcloud.com/content/celestyal-asset-share/us/en/light.html";
   iframe.width = '100%';
   iframe.height = '100%';
   iframe.style.border = 'none';
